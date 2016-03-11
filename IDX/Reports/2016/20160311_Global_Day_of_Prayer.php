66f03e3ba83f0cea94d4a21d637134ad0308412a
John Moore <jmoore@netazoic.com>
2016-03-11 15:44:40 -0800

    DOP 2016 report

---
+++ IDX/Reports/2016/20160311_Global_Day_of_Prayer.php
@@ -0,0 +1,24 @@
+<?php
+include_once("$_SERVER[DOCUMENT_ROOT]/_include/ch.php"); 
+
+?> 
+
+<p>Dear Friends, 
+
+<p>This year's Global Day of Prayer for Burma is this Sunday, March 13th. Join us as people around the world gather to pray for the nation of Burma, for freedom from oppression for people of all faiths and ethnicities, for guidance for the leaders of Burma, for justice and reconciliation, and that the hearts of all would be turned to love. 
+
+<p>We have made a prayer guide that is available on our website and there is a link to it below. Thank you for praying with us. 
+
+<p>God bless you,
+
+<p>Christians Concerned for Burma
+
+<p>
+
+<a href="http://www.prayforburma.org/IDX/Prayer/Day_of_Prayer/2016/DayOfPrayerForBurma2016.pdf">Day of Prayer PDF</a>
+
+
+<?php
+include_once("$_SERVER[DOCUMENT_ROOT]/_include/cf.php"); 
+
+?> 
