7cddd8cda041aa8a6c319ef9b77eb4f4d7021c37
John Moore <jomofrodo@gmail.com>
2017-03-09 00:26:09 -0500

    Removing special chars from 2017 DOP Report

--- IDX/Reports/2017/20170308_Global_Day_of_Prayer.php
+++ IDX/Reports/2017/20170308_Global_Day_of_Prayer.php
@@ -2,6 +2,8 @@
 include_once("$_SERVER[DOCUMENT_ROOT]/_include/ch.php"); 
 ?> 
 
+<em>8 March, 2017</em>
+
 <p>Dear Friends, 
 
 <p>This year marks the 20th anniversary of the Global Day of
@@ -17,7 +19,7 @@ through the gospel and their respective work. They cared
 for leprosy patients, young people, old people, handicapped
 people, students, refugees, and families caught in the crossfire.
 For the most part, their work was in Thailand, but it became
-apparent to them that Burma�s people, all of them, also were
+apparent to them that Burma's people, all of them, also were
 in great need of prayer. And so they began to add Burma to
 their prayers every Tuesday night, lifting up the people, the
 politicians, the ethnic groups, Aung San Suu Kyi and the
@@ -39,12 +41,12 @@ here in Burma especially, please pray for us. Don't be afraid
 because, "Perfect love casts out fear."
 
 <p>The final step was the encouragement of Dr. Saisuree
-Chutikul, a friend of Joan and Allan�s and also of Aung San Suu
+Chutikul, a friend of Joan and Allan's andalso of Aung San Suu
 Kyi's. Dave came back to Thailand with the idea of praying for
 Burma and then met Dr. Saisuree, a permanent senator in the
-Thai government and an advocate for women�s and children's
-rights and trafficking victims. She said, �You should start a day
-of prayer for Burma.� And so it began.
+Thai government and an advocate for women's and children's
+rights and trafficking victims. She said, "You should start a day
+of prayer for Burma." And so it began.
 
 <p>Please join us in praying for Burma on Sunday, March 12.
 
