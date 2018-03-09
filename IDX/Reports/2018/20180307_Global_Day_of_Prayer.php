7ede39df4edb615b9a55bd4bf92fbaf76a7159f4
John Moore <jmoore@netazoic.com>
2018-03-09 10:49:44 -0800

    Updates for announcement

--- IDX/Reports/2018/20180307_Global_Day_of_Prayer.php
+++ IDX/Reports/2018/20180307_Global_Day_of_Prayer.php
@@ -1,152 +1,48 @@
 <?php
 include_once("$_SERVER[DOCUMENT_ROOT]/_include/ch.php"); 
 ?> 
-
+<div style="float:left; width:300px; margin:10px;">
+<a href="http://www.prayforburma.org/IDX/Prayer/Day_of_Prayer/2018/2018 DOP English.pdf">
+<img src="http://www.prayforburma.org/IDX/Prayer/Day_of_Prayer/2018/images/Cover2018DOP.jpg"
+width="300"
+title="Day of Prayer for Burma, 2018" /></a>
+</div>
 <em>7 March, 2018</em>
 
 <p>Dear praying friends,
-Thank you for how you pray
-with faith and love. This past
-year the people of Burma have
-suffered many injustices: the
-murder, rape, and burning of
-villages and displacement of
-over 600,000 Rohingya people,
-the ongoing attacks against the
-Kachin, Ta'ang and northern
-Shan, the encroachment of the
-Burma military in ceasefire
-areas and the lack of care by the
-government for people who have
-been wronged.
-<p>
-During this time indigenous
-relief teams have responded
-in love and with help as best
-they can. We see relief teams
-bringing life-saving care to the
-displaced, pastors comforting
-their people and praying for
-justice and love, new medics and
-teachers being trained in the
-jungle to help their people, and
-the international community
-sounding the alarm for help
-for the oppressed. All these are
-answers to prayer in the face of
-evil.
-<p>
-In some ways Burma has
-improved. There is a civilian elected
-government and more
-freedoms in some areas, but
-the military holds ultimate
-power and uses it. We pray for
-the military to have a change
-of heart and for the elected
-government to have the
-courage to stand for all who are
-oppressed.
-<p>
-In the midst of this some from
-Burma have also been called to
-help in Iraq and Syria because
-of ISIS attacks there. How did
-people from Burma get to the
-Middle East and what does it
-have to do with Burma? We
-were invited because people
-knew we could help in war
-zones and we went because we
-believe God has no boundaries
-to helping each other.
 
-<p>In the bloody crucible
-of Mosul, Iraq, my family,
-volunteers, and some of our
-senior FBR ethnics from Burma
-learned lessons that we want to
-share in Burma and everywhere.
-These are not new lessons but
-they were burned into us by
-the desperate situations we
-saw, our dependency on God,
-and the spiritual battle we saw
-overarching the physical battle.
+<p>Thank you for joining us on March 11 in praying for Burma. This past year the people of Burma have suffered many injustices; the murder, rape, and burning of villages and displacement of over 700,000 Rohingya people, the ongoing attacks against the Kachin, Ta’ang and northern Shan, and the lack of care by the government for people who have been wronged.
 
-<p>In the midst of this God showed
-us four things.
+<p>In some ways Burma has improved. There is a civilian-elected government and more freedoms in some areas, but the military holds ultimate power and uses it. We pray for the military to have a change of heart and for the elected government to have the courage to stand for all who are oppressed. God has taught us lessons that we want to share in Burma and everywhere.
 
-<p>First, wherever Jesus sends
-you, He goes with you. So when
-we are afraid we can pray and
-ask, "Jesus is this really your
-will?" Then if it is still a 'yes', go
-boldly in His name and use His
-name to overcome evil.
+<p>First, wherever Jesus sends you, He goes with you. Second, when we are faced with great evil, Jesus will work through our weakness and bring good from the clutches of evil. Third, vengeance is often an attempt to get justice in a spirit of hate and retribution but justice can only be achieved with love and mercy. We need God’s help to love those who wrong us, to love our enemies – and when we pray and ask for this love He helps us. Four, we experienced the power of the praying church and believe that God’s power through prayer is the only way to achieve freedom, justice, reconciliation and love.
 
-<p>Second, when faced with great
-evil, we sinful humans cannot
-defeat it. But if we are following
-Jesus and call on His name, He
-comes through our weakness
-and brings good out of the
-clutches of evil.
+<p>The way forward in Burma is to pray and ask Jesus to help us love and help us know where to go, how to go and what to do. Thank you for praying with us for all the people of Burma.
 
+<p>God bless you,
 
-<p>Third, what is the difference
-between justice and vengeance?
-I believe vengeance is an
-attempt to get justice with a
-spirit of hate and retribution.
-When you suffer injustice, the
-hurt can cause you to hate the
-perpetrator and to equate their
-hurt with justice -- but hate will
-cripple you if you search for
-justice this way, and justice will
-elude you. Justice can only be
-achieved with love and mercy.
-This means when you are
-wronged you pray for love for
-the perpetrator and you seek
-justice in that love. We need
-God's help to love those who
-wrong us, to love our enemies
--- but when we pray and ask for
-this love He helps us. This may
-mean punishment for the person
-who wronged you but it will
-be done in a spirit of love. This
-spirit keeps us free and leads
-towards reconciliation.
-
-<p>Four, we experienced the
-power of the praying church
-and believe that God's power
-through prayer is the only way
-to achieve freedom, justice,
-reconciliation and love.
-In Burma as in other places
-there is hate, murder, and all
-manner of injustice. The way
-forward is to pray and ask Jesus
-to help us love and help us know
-where to go, how to go and what
-to do. And then wherever He
-sends us, Jesus goes with us, so
-we can go humbly, boldly and in
-His love.
-
-<p>Thank you for praying with us
-for all the people of Burma. God
-bless you.
-
-<p>Dave Eubank
+<p>Dave Eubank 
 <br/>Christians Concerned for Burma
-<p>
 
-<a href="http://www.prayforburma.org/IDX/Prayer/Day_of_Prayer/2018/2018 DOP English.pdf">Click here to download the Day of Prayer PDF</a>
+
+                            <table width="100%" >
+                               <tr bgcolor="#FFFFFF"> 
+                                <td colspan="2"> 
+									  <ul style="list-style: none;">
+									<li><a href="http://www.prayforburma.org/IDX/Prayer/Day_of_Prayer/2018/2018 DOP English.pdf"><img src="http://www.prayforburma.org/images/pdficonsmall.gif"  /> Day of Prayer 2018 (English)</a> 24 Mb</li>
+									<li><a href="http://www.prayforburma.org/IDX/Prayer/Day_of_Prayer/2018/2018 DOP Burmese.pdf"><img src="http://www.prayforburma.org/images/pdficonsmall.gif" /> sutaungghkyinnsai 2018 nae (Burmese)</a> 46 Mb</li>
+									<li><a href="http://www.prayforburma.org/IDX/Prayer/Day_of_Prayer/2018/2018 DOP Karen.pdf"><img src="http://www.prayforburma.org/images/pdficonsmall.gif" /> Day of Prayer 2018 (Karen)</a> 40 Mb</li>
+									<li><a href="http://www.prayforburma.org/IDX/Prayer/Day_of_Prayer/2018/2018 DOP Thai.pdf"><img src="http://www.prayforburma.org/images/pdficonsmall.gif" /> Wạn s̄wd mnt̒ 2018(Thai)</a> 1 Mb</li>
+									<li><a href="http://www.prayforburma.org/IDX/Prayer/Day_of_Prayer/2018/2018 DOP Danish.pdf"><img src="http://www.prayforburma.org/images/pdficonsmall.gif" /> Dag i Bøn 2018 (Danish)</a> 24 Mb</li>
+									</ul>
+                              <tr bgcolor="#FFFFFF"> 
+                                <td colspan="2"> 
+
+                              <tr> 
+                                <td colspan="2"> 
+                              </tr>
+                            </table>
+
 
 
 <?php
